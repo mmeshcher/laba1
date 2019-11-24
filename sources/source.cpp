0d63f56d8d156a9b63617b65d44f8d199bac5d2c
mmeshcher <keyvrn12012014@icloud.com>
2019-11-24 21:51:32 +0300

    unit

--- sources/source.cpp
+++ sources/source.cpp
@@ -1,5 +1,13 @@
-// Copyright 2018 Your Name <your_email>
+// Copyright 2019 mmeshcher
+#include "Json.hpp"
+using namespace std;
+int main() {
+  string text;
+  text =
+      R"({ "lastname" : "Ivanov" , "firstname" : "Ivan" , "age" : 25,"islegal" : false,"marks" : [ 4 , 5 , 5 , 5 , 2 , 3 ],"address" : {  "city" : "Moscow" ,  "street" : "Vozdvijenka" } })";
+  Json object = Json::parseFile(text);
+  cout << endl;
+  if (object.is_object()) cout << "Yes";
 
-// Here will be my code
-
-#include <header.hpp>
+  return 0;
+}
