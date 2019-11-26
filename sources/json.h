2406dc1ad8c4c709774d8ac2965f9fe99d8de279
mmeshcher <keyvrn12012014@icloud.com>
2019-11-26 23:03:09 +0300

    +

--- sources/json.h
+++ sources/json.h
@@ -9,73 +9,74 @@
 
 
 enum Type {
-	JSTRING,
-	JOBJECT,
-	JARRAY,
-	JBOOLEAN,
-	JNUMBER,
-	JNULL,
-	JUNKNOWN
+    JSTRING,
+    JOBJECT,
+    JARRAY,
+    JBOOLEAN,
+    JNUMBER,
+    JNULL,
+    JUNKNOWN
 };
 
 enum TokenType {
-	UNKNOWN,
-	STRING,
-	NUMBER,
-	CROUSH_OPEN,
-	CROUSH_CLOSE,
-	BRACKET_OPEN,
-	BRACKET_CLOSE,
-	COMMA, COLON,
-	BOOLEAN,
-	NUL
+    UNKNOWN,
+    STRING,
+    NUMBER,
+    CROUSH_OPEN,
+    CROUSH_CLOSE,
+    BRACKET_OPEN,
+    BRACKET_CLOSE,
+    COMMA, COLON,
+    BOOLEAN,
+    NUL
 };
 
 struct Token {
-	std::string m_value;
-	TokenType m_type;
-	Token(std::string value = "", TokenType type = UNKNOWN) : m_value(value), m_type(type) {}
+    std::string m_value;
+    TokenType m_type;
+    Token(std::string value = "", TokenType type = UNKNOWN) : m_value(value), m_type(type) {}
 };
 
 class Json {
 public:
-	Json();
-	Json(const std::string& s);
+    Json();
+    Json(const std::string& s);
 
-	static Json parse(const std::string& str);
-	static Json parseFile(const std::string& str);
+    static Json parse(const std::string& str);
+    static Json parseFile(const std::string& str);
 
-	bool is_array() const;
+    bool is_array() const;
 
-	bool is_object() const;
+    bool is_object() const;
 
-	std::any& operator[](const std::string& s);
-	std::any& operator[](int i);
+    std::any& operator[](const std::string& s);
+    std::any& operator[](int i);
 
 private:
-	static bool IsWhitespace(const char c);
-	static int NextWhitespace(const std::string& source, int i);
-	static int SkipWhitespaces(const std::string& source, int i);
+	
+    static bool IsWhitespace(const char c);
+    static int NextWhitespace(const std::string& source, int i);
+    static int SkipWhitespaces(const std::string& source, int i);
 
-	static std::vector<Token> Tokenize(std::string source);
+    static std::vector<Token> Tokenize(std::string source);
 
-	static Json JsonParse(std::vector<Token> v, int i, int& r);
+    static Json JsonParse(std::vector<Token> v, int i, int& r);
 
-	void set_type(Type tp);
-	void add_property(std::string new_key, Json v);
-	void add_element(Json v);
-	void set_string(std::string s);
+    void set_type(Type tp);
+    void add_property(std::string new_key, Json v);
+    void add_element(Json v);
+    void set_string(std::string s);
 
-	std::any ConvertType(const std::string& str, Type type);
+    std::any ConvertType(const std::string& str, Type type);
 
 protected:
-	std::string key;
-	Type type;
+    std::string key;
+    Type type;
 
 private:
-	std::any value;
-	std::vector<std::pair<std::string, std::any>> prop;
-	std::vector<std::any> arr;
+    std::any value;
+    std::vector<std::pair<std::string, std::any>> prop;
+    std::vector<std::any> arr;
 };
 
 #endif // __JSON_H__
